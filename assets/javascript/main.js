7b103f47b9c048dd0abe446acfdcca39c37956ac
unknown <Mendez.AndrewM@gmail.com>
2019-07-17 21:13:24 -0700

    added timing functions for nav clicks

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,67 +1,74 @@
 $(document).ready(function () {
-// Materialize function for sidebar functionality
+  // Materialize function for sidebar functionality
   $('.sidenav').sidenav();
   $(".hide-at-start").hide()
-let activepage;
+  let activePage = $("#start");
 
 
-function homeToProfile() {
-  $("#start").hide()
-  $("#bio-page").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
-  activepage = $("#bio-page");
-}
-function homeToPortfolio() {
-  $("#start").hide()
-  $("#portfolio").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
-  activepage = $("#portfolio-page");
-}
+  function toProfile() {
+    activePage.hide()
+    $("#bio-page").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
+    activePage = $("#bio-page");
+  }
+  function toPortfolio() {
+    activePage.hide()
+    $("#portfolio").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
+    activePage = $("#portfolio-page");
+  }
+  function toContact() {
+    activePage.hide();
+    $("#contact").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
+    activePage = $("#contact");
+  }
+  function toStart() {
+    $("#start").show().removeClass("deactivated").addClass("activated") //To Be Rplaced
+    activePage = $("#start");
+  }
 
-// Click Events from Home Page 
+  // Click Events from Home Page 
   $("#profile-btn").click(function () {
-  setTimeout(homeToProfile, 200)
-  $("#start").addClass("deactivated")
-  $("#navBar").removeClass("deactivated").addClass("activated")
-  $("footer").removeClass("deactivated").addClass("activated")
-  // TODO: Make Bio Page Slide in from the Right
-});
+    setTimeout(toProfile, 200)
+    $("#start").addClass("deactivated")
+    $("#navBar").removeClass("deactivated").addClass("activated")
+    $("footer").removeClass("deactivated").addClass("activated")
+    // TODO: Make Bio Page Slide in from the Right
+  });
 
-$("#portfolio-btn").click(function () {
-  setTimeout(homeToPortfolio, 200)
-  $("#start").addClass("deactivated")
-  $("#navBar").removeClass("deactivated").addClass("activated")
-  $("footer").removeClass("deactivated").addClass("activated")
-  // TODO: Make Portfolio Page Slide in from the Right
-});
+  $("#portfolio-btn").click(function () {
+    setTimeout(toPortfolio, 200)
+    $("#start").addClass("deactivated")
+    $("#navBar").removeClass("deactivated").addClass("activated")
+    $("footer").removeClass("deactivated").addClass("activated")
+    // TODO: Make Portfolio Page Slide in from the Right
+  });
 
 
 
-// Nav Bar Click Events
-$(".nav-home").click(function() {
-  activepage.hide();
-  $("#start").show().removeClass("deactivated").addClass("activated")
-  activepage = $("#start") 
-})
-$(".nav-about").click(function() {
-  activepage.hide();
-  $("#bio-page").show().removeClass("deactivated").addClass("activated")
-  activepage = $("#bio-page") 
-})
-$(".nav-portfolio").click(function() {
-  activepage.hide();
-  $("#portfolio").show().removeClass("deactivated").addClass("activated")
-  activepage = $("#portfolio") 
-})
-$(".nav-contact").click(function() {
-  activepage.hide();
-  $("#contact").show().removeClass("deactivated").addClass("activated")
-  activepage = $("#contact") 
-})
+  // Nav Bar Click Events
+  $(".nav-home").click(function () {
+    activePage.removeClass("activated").addClass("deactivated");
+    $("#navBar").removeClass("activated").addClass("deactivated")
+    $("footer").removeClass("activated").addClass("deactivated")
+    setTimeout(toStart, 200)
+  })
+  $(".nav-about").click(function () {
+    activePage.removeClass("activated").addClass("deactivated");
+    setTimeout(toProfile, 200)
+  })
+  $(".nav-portfolio").click(function () {
+    activePage.removeClass("activated").addClass("deactivated");
+    setTimeout(toPortfolio, 200)
+  })
+  $(".nav-contact").click(function () {
+    activePage.removeClass("activated").addClass("deactivated");
+    setTimeout(toContact, 200)
+  })
 
-// Contact Page 
+  // Contact Page 
 
-$("#contact-submit").click(function() {
-  alert("Feature Coming Soon")
-})
+  $("#contact-submit").click(function () {
+    alert("Feature Coming Soon")
+  })
 
 
 
